651108d2b61dff5daf898e341de37fff45e02dd4
Sean Soria <github@seansoria.com>
2011-03-10 17:14:10 -0800

    fix spacing in setup.py

--- setup.py
+++ setup.py
@@ -1,44 +1,44 @@
- """
- py2app/py2exe build script for MyApplication.
+"""
+py2app/py2exe build script for MyApplication.
 
- Will automatically ensure that all build prerequisites are available
- via ez_setup
+Will automatically ensure that all build prerequisites are available
+via ez_setup
 
- Usage (Mac OS X):
-     python setup.py py2app
+Usage (Mac OS X):
+    python setup.py py2app
 
- Usage (Windows):
-     python setup.py py2exe
- """
- import ez_setup
- ez_setup.use_setuptools()
+Usage (Windows):
+    python setup.py py2exe
+"""
+import ez_setup
+ez_setup.use_setuptools()
 
- import sys
- from setuptools import setup
+import sys
+from setuptools import setup
 
- mainscript = 'network-client.py'
+mainscript = 'network-client.py'
 
- if sys.platform == 'darwin':
-     extra_options = dict(
-         setup_requires=['py2app'],
-         app=[mainscript],
-         # Cross-platform applications generally expect sys.argv to
-         # be used for opening files.
-         options=dict(py2app=dict(argv_emulation=True)),
-     )
- elif sys.platform == 'win32':
-     extra_options = dict(
-         setup_requires=['py2exe'],
-         app=[mainscript],
-     )
+if sys.platform == 'darwin':
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=[mainscript],
+        # Cross-platform applications generally expect sys.argv to
+        # be used for opening files.
+        options=dict(py2app=dict(argv_emulation=True)),
+    )
+elif sys.platform == 'win32':
+    extra_options = dict(
+        setup_requires=['py2exe'],
+        app=[mainscript],
+    )
 else:
-     extra_options = dict(
-         # Normally unix-like platforms will use "setup.py install"
-         # and install the main script as such
-         scripts=[mainscript],
-     )
+    extra_options = dict(
+        # Normally unix-like platforms will use "setup.py install"
+        # and install the main script as such
+        scripts=[mainscript],
+    )
 
 setup(
-    name="MyApplication",
-    **extra_options
+   name="MyApplication",
+   **extra_options
 )
