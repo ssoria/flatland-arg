5114fe285fe6ba3212348fe0f41ccead3503e76d
Sean Soria <github@seansoria.com>
2011-03-22 23:38:44 -0700

    Animation class to handle multi-png animations

---
+++ game/animation.py
@@ -0,0 +1,32 @@
+from twisted.internet.task import LoopingCall
+import pygame
+
+def loadImage(path):
+    image = pygame.image.load(path)
+    image = image.convert()
+    image.set_colorkey(image.get_at((0,0)))
+    return image
+
+class Animation(object):
+    def __init__(self, path):
+        i = 0
+        self._images = {}
+        while True:
+            try:
+                self._images[i] = loadImage(path.format(i + 1))
+                i += 1
+            except Exception as e:
+                break
+        self.width, self.height = self._images[0].get_size()
+
+    def draw(self, screen, position):
+        position = (position[0] - (self.width / 2), position[1] - (self.height / 2))
+        screen.blit(self._images[self._imageIndex], position)
+
+    def start(self, fps):
+        self._imageIndex = 0
+        self._loopingCall = LoopingCall(self._incrementImageIndex)
+        self._loopingCall.start(1.0 / fps)
+
+    def _incrementImageIndex(self):
+        self._imageIndex = (self._imageIndex + 1) % len(self._images)
