710900b9d2b0b72a015acb0713593aa3d6b87901
Sean Soria <github@seansoria.com>
2011-03-31 00:06:01 -0700

    clean up Window class methods

--- game/view.py
+++ game/view.py
@@ -50,36 +50,6 @@ class Window(object):
         self.action = None
         self.startAction()
 
-    def draw(self, image, position):
-        """
-        Render an image at a position.
-        """
-        viewCoord = self.viewport.modelToView(position)
-        self.screen.blit(image, (viewCoord.x, viewCoord.y - image.get_size()[1]))
-
-    def drawPlayer(self, position, sides, resources):
-        """
-        Render a player at a position
-        """
-        x, y = self.viewport.modelToView(position).tuple()
-        pygame.draw.circle(self.screen, (255, 255, 255), (x, y), 50)
-
-        while resources > 0:
-            pygame.draw.line(self.screen, (0, 255, 0), (x, y), (x, y+10), 7)
-            x += 10
-            resources -= 1
-            sides -= 1
-        while sides > 0:
-            pygame.draw.line(self.screen, (255, 0, 0), (x, y), (x, y+10), 7)
-            x += 10
-            sides -= 1
-
-
-    def drawResourcePool(self):
-        x, y = self.resourcePool.position.tuple()
-        pygame.gfxdraw.filled_circle(self.screen, x, y, self.resourcePool.size, pygame.Color(0, 0, 255, 150))
-
-
     def paint(self):
         """
         Call C{paint} on all views which have been directly added to
