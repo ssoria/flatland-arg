5ac726835799cb581ab8a7b279169b45eb7d7382
sean <sean.soria@gmail.com>
2011-10-04 20:58:34 -0700

    trackerApp1

--- game/tracker.py
+++ game/tracker.py
@@ -33,79 +33,107 @@ def calculate_points(x1, y1, x2, y2, steps=5):
 
 
 class Tracker(FloatLayout):
-
     def connect(self):
-        self._lost_targets = []
-        self._new_targets = []
-        self._moved_targets = []
-        self._clientfactory = pb.PBClientFactory()
-        reactor.connectTCP("localhost", 8789, self._clientfactory)
-        d = self._clientfactory.getRootObject()
-        d.addCallback(self.send_msg)
+        win = self.get_parent_window()
+        self.real_start_x = 10
+        self.real_start_y = 10
+        self.real_height = self.height - 20
+        self.real_width = self.width - 20
 
-    def send_msg(self, result):
-        result.callRemote("echo", "hello network")
+        print self.real_height, self.real_width
 
-    def send_lost_target(self, result):
-        result.callRemote("lost_target", self._lost_targets.pop())
+        self._ready = False;
+        self._corners = [];
+        pass
 
-    def send_new_target(self, result):
-        result.callRemote("new_target", self._new_targets.pop())
+    def _init_corner(self, touch):
+        self._corners.append(touch)
+        if len(self._corners) == 4:
+            self._init_keystone()
+            self._ready = True
 
-    def send_target_moved(self, result):
-        result.callRemote("moved_target", self._moved_targets.pop())
+    def _init_keystone(self):
+        self._corners.sort(key = lambda touch: touch.y)
 
-    def get_msg(self, result):
-        print "server echoed: ", result
+        if self._corners[0].x > self._corners[1].x:
+            temp = self._corners[0]
+            self._corners[0] = self._corners[1]
+            self._corners[1] = temp
+
+        if self._corners[2].x > self._corners[3].x:
+            temp = self._corners[2]
+            self._corners[2] = self._corners[3]
+            self._corners[3] = temp
+
+        self.start_y = self._corners[0].y
+        self.height1 = self._corners[2].y - self.start_y
+
+        self.start_x1 = self._corners[0].x
+        self.start_x2 = self._corners[2].x
+        self.width1 = self._corners[1].x - self._corners[0].x
+        self.width2 = self._corners[3].x - self._corners[2].x
+
+        print (self._corners[0].x, self._corners[0].y)
+        print (self._corners[1].x, self._corners[1].y)
+        print (self._corners[2].x, self._corners[2].y)
+        print (self._corners[3].x, self._corners[3].y)
+
+
+    def rectify(self, touch):
+        field_percent = (touch.y - self.start_y) / self.height1
+        y = self.real_start_y + (self.real_height * field_percent)
+
+        start_x = (1 - field_percent) * self.start_x1 + field_percent * self.start_x2
+        width = (1 - field_percent) * self.width1 + field_percent * self.width2
+        x = self.real_start_x + (self.real_width * ((touch.x - start_x) / width))
+
+        print (touch.x, touch.y)
+        print x, y
+        return x, y
+
 
     def on_touch_down(self, touch):
-        win = self.get_parent_window()
+        ud = touch.ud
+        ud['group'] = g = str(touch.uid)
+
+        if not self._ready:
+            self._init_corner(touch)
+            return;
+
+        x, y = self.rectify(touch)
+
         ud = touch.ud
         ud['group'] = g = str(touch.uid)
         with self.canvas:
             ud['color'] = Color(random(), 1, 1, mode='hsv', group=g)
-            ud['lines'] = (
-                Rectangle(pos=(touch.x, 0), size=(1, win.height), group=g),
-                Rectangle(pos=(0, touch.y), size=(win.width, 1), group=g),
-                Point(points=(touch.x, touch.y), source='particle.png',
-                      pointsize=5, group=g))
-
-        ud['label'] = Label(size_hint=(None, None))
-        self.update_touch_label(ud['label'], touch)
-        self.add_widget(ud['label'])
-        self._new_targets.append({'id': touch.uid, 'position': {'x': touch.x, 'y': touch.y}})
-        d = self._clientfactory.getRootObject()
-        d.addCallback(self.send_new_target)
+            ud['lines'] = Point(
+                points = (x, y),
+                source = 'particle.png',
+                pointsize = 5,
+                group=g)
 
     def on_touch_move(self, touch):
+        return
+
+        if not self._ready:
+            return
+
+        x, y = self.rectify(touch)
+
         ud = touch.ud
-        ud['lines'][0].pos = touch.x, 0
-        ud['lines'][1].pos = 0, touch.y
 
-        points = ud['lines'][2].points
-        oldx, oldy = points[-2], points[-1]
-        points = calculate_points(oldx, oldy, touch.x, touch.y)
-        if points:
-            try:
-                lp = ud['lines'][2].add_point
-                for idx in xrange(0, len(points), 2):
-                    lp(points[idx], points[idx+1])
-            except GraphicException:
-                pass
-
-        ud['label'].pos = touch.pos
-        self.update_touch_label(ud['label'], touch)
-        self._moved_targets.append({'id': touch.uid, 'position': {'x': touch.x, 'y': touch.y}})
-        d = self._clientfactory.getRootObject()
-        d.addCallback(self.send_target_moved)
+        try:
+            ud['lines'] = Point(
+                points = (x, y),
+                source = 'particle.png',
+                pointsize = 5,
+                group=ud['group'])
+        except GraphicException:
+            pass
 
     def on_touch_up(self, touch):
         ud = touch.ud
-        self.canvas.remove_group(ud['group'])
-        self.remove_widget(ud['label'])
-        self._lost_targets.append({'id': touch.uid, 'position': {'x': touch.x, 'y': touch.y}})
-        d = self._clientfactory.getRootObject()
-        d.addCallback(self.send_lost_target)
+        #self.canvas.remove_group(ud['group'])
 
     def update_touch_label(self, label, touch):
         label.text = 'ID: %s\nPos: (%d, %d)\nClass: %s' % (
@@ -113,7 +141,6 @@ class Tracker(FloatLayout):
         label.texture_update()
         label.pos = touch.pos
         label.size = label.texture_size[0] + 20, label.texture_size[1] + 20
-
 
 
 class TrackerApp(App):
